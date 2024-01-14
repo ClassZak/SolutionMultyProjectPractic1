a9efdb7246adfa2ab79ce70e7f9e9417b2b4c4e4
pahar <pahar@DESKTOP-F6KCJ9A>
2024-01-14 19:39:56 +0300

    Добавьте файлы проекта.

---
+++ ProjectManager/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Button button;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] resultBuffer=new string[1];
+            ProjectOne.Calendar.Main(resultBuffer);
+            MessageBox.Show(resultBuffer[0]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProjectTwo.Form1 form1 = new ProjectTwo.Form1();
+            form1.Show();
+        }
+    }
+}
